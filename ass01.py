df00c9d2ad93c23e1cdef350c289bba48a0c711d
Preethika Reddy <106732526+pothulapreethikareddy@users.noreply.github.com>
2024-10-03 22:54:50 -0700

    Add files via upload

--- ass01.py
+++ ass01.py
@@ -1,4 +1,3 @@
+"string reverse "
 s="learning python is very easy"
-word=s.split()
-reversed_sentence=word[4]+' '+word[3]+' '+word[2]+' '+word[1]+' '+word[0]+' '
-print(reversed_sentence)
+print(s[::-1])
